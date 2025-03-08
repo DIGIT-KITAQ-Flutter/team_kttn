e8d9050e5a68a6510cb459c9d5132f3b86463763
tanaka-0224 <r.iku0224@icloud.com>
2025-03-08 10:40:38 +0900

    #12の変更が反映されていなかったのでこのcommitで反映

--- lib/login/auth_gate.dart
+++ lib/login/auth_gate.dart
@@ -1,5 +1,4 @@
-import 'package:digit_kttn/chat/chat.dart';
-import 'package:digit_kttn/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:digit_kttn/root/root_page.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
@@ -7,6 +6,24 @@ import 'package:flutter/material.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
+
+  /// Firestore にユーザー情報を保存するメソッド
+  Future<void> saveUserToFirestore(User user) async {
+    final userRef =
+        FirebaseFirestore.instance.collection('users').doc(user.uid);
+
+    // Firestore に既にデータがあるか確認
+    final docSnapshot = await userRef.get();
+    if (!docSnapshot.exists) {
+      // ユーザー情報を Firestore に保存
+      await userRef.set({
+        'uid': user.uid,
+        'email': user.email,
+        'icon_url': 'assets/images/default_icon.png', // デフォルトアイコンURL（必要に応じて変更）
+        'created_record': FieldValue.serverTimestamp(),
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +37,11 @@ class AuthGate extends StatelessWidget {
             ],
           );
         }
+
+        // ログインユーザーがいる場合、Firestore にユーザー情報を保存
+        final user = snapshot.data!;
+        saveUserToFirestore(user);
+
         return const RootPage();
       },
     );
